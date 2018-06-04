69ddfc26080970207ffef05a7fdc718134f1f386
Matthias Bogaert <hello@matthiasbogaert.be>
2018-06-04 23:55:52 +0200

    Initial Commit

---
+++ Collector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collector
+//
+//  Created by De Winter Laura on 4/06/18.
+//  Copyright © 2018 Comflow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
